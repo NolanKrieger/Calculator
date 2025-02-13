cbd5e2a3680e629b9569d08e30994827c1d1ea79
NolanKrieger <nolankrieger9@gmail.com>
2025-02-12 23:58:02 -0500

    bug fixes

--- Calculator.java
+++ Calculator.java
@@ -21,18 +21,21 @@ public class Calculator{
             System.out.println(first - second);
         }
 
+     
+
         System.out.println("Would you like to perform multiplication or division?");
 
         String secondChoice = mainScan.nextLine();
 
-        if(firstChoice.toLowerCase().equals("multiplication")){
+
+        if(secondChoice.toLowerCase().equals("multiplication")){
             System.out.println(first * second);
-        }else if(firstChoice.toLowerCase().equals("division")){
+        }else if(secondChoice.toLowerCase().equals("division")){
             if(second == 0){
                 System.out.println("Error: Divide by Zero");
                 return;
             }
-            System.out.println((double)(first / second));
+            System.out.println((double)first / second);
         }
 
     }
