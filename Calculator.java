105552c090127263d2e3cd7d821cea4e69cf222d
NolanKrieger <nolankrieger9@gmail.com>
2025-02-12 20:46:54 -0500

    asked user to enter 2 numbers and added them together

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,13 @@ public class Calculator{
     public static void main(String[] args){
 
         Scanner mainScan = new Scanner(System.in);
-        
+
+        System.out.println("Enter two whole numbers");
+
+        int first = mainScan.nextInt();
+        int second = mainScan.nextInt();
+
+        System.out.println(first + second);
 
     }
 }
